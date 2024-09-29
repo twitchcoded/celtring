fba66ee765ef81eb7fc88aa32ca73c136d271651
twitchcoded <twitchcoded@gmail.com>
2024-09-29 14:13:58 +0100

    Update onionring-variables.js

--- onionring-variables.js
+++ onionring-variables.js
@@ -7,19 +7,17 @@
 
 //the full URLs of all the sites in the ring
 var sites = [
+'https://twitch.cymru/',
 'https://example.com/',
-'https://example.com/',
-'https://example.com/',
-'https://example.com/'
 ];
 
 //the name of the ring
-var ringName = 'My Ring';
+var ringName = 'Modern Celtic Languages Webring';
 
 /* the unique ID of the widget. two things to note:
  1) make sure there are no spaces in it - use dashes or underscores if you must
  2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
-var ringID = 'my-ring';
+var ringID = 'celtring';
 
 //should the widget include a link to an index page?
 var useIndex = false;
